4cfd62cfde3a6a83e1d3de269515fdfabd79e940
darbestor <fordarbe@gmail.com>
2023-01-20 13:59:37 +0600

    triangle

---
+++ ShapesFilter/Shapes/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShapesFilter.Shapes
+{
+    public class Triangle : Polygon
+    {
+        public Triangle(params PointF[] vertices) : base(vertices)
+        {
+            if (vertices.Length != 3)
+            {
+                throw new ArgumentException("Triangle must have 3 vertices");
+            }
+        }
+
+        public Triangle(IEnumerable<PointF> boundaries) : this(boundaries.ToArray())
+        {
+        }
+    }
+}
