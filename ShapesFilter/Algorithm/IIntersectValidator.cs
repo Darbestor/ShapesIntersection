c1b85fb1e4000128b67bf8ba84149733a468a3c6
darbestor <fordarbe@gmail.com>
2023-01-20 14:17:40 +0600

    validator interface

---
+++ ShapesFilter/Algorithm/IIntersectValidator.cs
@@ -0,0 +1,11 @@
+﻿using ShapesFilter.Shapes;
+
+namespace ShapesFilter.Algorithm
+{
+    public interface IIntersectValidator<S1, S2>
+        where S1 : IShape
+        where S2 : IShape
+    {
+        bool Intersect(S1 shape1, S2 shape2);
+    }
+}
