18e610e9eeb81eb3c743cc41f84b284675b15aec
darbestor <fordarbe@gmail.com>
2023-01-20 18:34:56 +0600

    line rectangle intersect

---
+++ ShapesFilter/Algorithms/LineIntersectsRectangle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using ShapesFilter.Algorithm;
+using ShapesFilter.Shapes;
+
+namespace ShapesFilter.Algorithms
+{
+    public class LineIntersectsRectangle : IIntersectValidator<Line, Rectangle>
+    {
+        public bool Intersect(Line line, Rectangle rectangle)
+        {
+            // Find min and max X for the segment
+            double minX = line.P1.X;
+            double maxX = line.P2.X;
+
+            if (line.P1.X > line.P2.X)
+            {
+                minX = line.P2.X;
+                maxX = line.P1.X;
+            }
+
+            // Find the intersection of the segment's and rectangle's x-projections
+            if (maxX > rectangle.BottomRight.X)
+            {
+                maxX = rectangle.BottomRight.X;
+            }
+
+            if (minX < rectangle.TopLeft.X)
+            {
+                minX = rectangle.TopLeft.X;
+            }
+
+            if (minX > maxX) // If their projections do not intersect return false
+            {
+                return false;
+            }
+
+            // Find corresponding min and max Y for min and max X we found before
+            double minY = line.P1.Y;
+            double maxY = line.P2.Y;
+
+            double dx = line.P2.X - line.P1.X;
+
+            if (Math.Abs(dx) > 0.0000001)
+            {
+                double a = (line.P2.Y - line.P1.Y) / dx;
+                double b = line.P1.Y - a * line.P1.X;
+                minY = a * minX + b;
+                maxY = a * maxX + b;
+            }
+
+            if (minY > maxY)
+            {
+                (maxY, minY) = (minY, maxY);
+            }
+
+            // Find the intersection of the segment's and rectangle's y-projections
+            if (maxY > rectangle.BottomRight.Y)
+            {
+                maxY = rectangle.BottomRight.Y;
+            }
+
+            if (minY < rectangle.TopLeft.Y)
+            {
+                minY = rectangle.TopLeft.Y;
+            }
+
+            if (minY > maxY) // If Y-projections do not intersect return false
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ShapesFilterTests/Algorithms/LineIntersectsRectangleTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ShapesFilter.Algorithms;
+using ShapesFilter.Shapes;
+
+namespace ShapesFilterTests.Algorithms
+{
+    public class LineIntersectsRectangleTests
+    {
+        private static IEnumerable<TestCaseData> IntersectCases()
+        {
+            yield return new TestCaseData(new Line(150, 0, 150, 150), new Rectangle(100, 100, 100, 100));
+            yield return new TestCaseData(new Line(150, 0, 150, 100), new Rectangle(100, 100, 100, 100));
+            yield return new TestCaseData(new Line(150, 0, 150, 300), new Rectangle(100, 100, 100, 100));
+            yield return new TestCaseData(new Line(120, 50, 70, 300), new Rectangle(100, 100, 100, 100));
+        }
+        
+        [Test]
+        [TestCaseSource(nameof(IntersectCases))]
+        public void TestIntersect(Line line, Rectangle rectangle)
+        {
+            var alg = new LineIntersectsRectangle();
+            
+            Assert.True(alg.Intersect(line, rectangle));
+        }
+        
+        [Test]
+        public void TestContains()
+        {
+            var line = new Line(120, 110, 170, 180);
+            var rect = new Rectangle(100, 100, 100, 100);
+
+            
+            var alg = new LineIntersectsRectangle();
+            
+            Assert.True(alg.Intersect(line, rect));
+        }
+
+        private static IEnumerable<TestCaseData> DoNotIntersectCases()
+        {
+            yield return new TestCaseData(new Line(100, 30, 30, 30), new Rectangle(100, 100, 100, 100));
+            yield return new TestCaseData(new Line(100, 30, 200, 30), new Rectangle(100, 100, 100, 100));
+            yield return new TestCaseData(new Line(99, 30, 99, 30), new Rectangle(100, 100, 100, 100));
+        }
+        
+        [Test]
+        [TestCaseSource(nameof(DoNotIntersectCases))]
+        public void TestDoNotIntersect(Line line, Rectangle rectangle)
+        {
+            var alg = new LineIntersectsRectangle();
+            
+            Assert.False(alg.Intersect(line, rectangle));
+        }
+        
+    }
+}
