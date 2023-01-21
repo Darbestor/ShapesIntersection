2e1a72d7d52756c18508627d247ce6696a5e2d6d
darbestor <fordarbe@gmail.com>
2023-01-21 14:40:04 +0600

    df

---
+++ ShapesFilter/DI/StrategySelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ShapesFilter.Algorithms;
+using ShapesFilter.Algorithms.LineIntersections;
+using ShapesFilter.Shapes;
+
+namespace ShapesFilter.DI
+{
+    class StrategySelector<S1, S2>
+        where S1 : IShape
+        where S2 : IShape
+    {
+        private readonly Dictionary<(ShapeType, ShapeType), IIntersectValidator<S1, S2>> _validators;
+
+        public StrategySelector()
+        {
+            _validators = new Dictionary<(ShapeType, ShapeType), IIntersectValidator<S1, S2>>
+            {
+                { (ShapeType.Line, ShapeType.Line), (IIntersectValidator<S1, S2>)new LineIntersectsLine() }
+            };
+        }
+
+
+        public IIntersectValidator<S1, S2> GetStrategy(S1 shape1, S2 shape2)
+        {
+            return _validators.TryGetValue((shape1.ShapeType, shape2.ShapeType), out var v) ? v : null;
+        }
+    }
+}
