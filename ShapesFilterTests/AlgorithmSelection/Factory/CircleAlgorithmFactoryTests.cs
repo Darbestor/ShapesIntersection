d81c801b4d52b71965fe0ebb035903f4f37a3eaf
darbestor <fordarbe@gmail.com>
2023-01-22 19:06:25 +0600

    factories tests

---
+++ ShapesFilterTests/AlgorithmSelection/Factory/CircleAlgorithmFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ShapesFilter.Algorithms;
+using ShapesFilter.AlgorithmSelection.Factory;
+using ShapesFilter.AlgorithmSelection.Strategies;
+using ShapesFilter.Shapes;
+
+namespace ShapesFilterTests.AlgorithmSelection.Factory
+{
+    [TestFixture]
+    public class CircleAlgorithmFactoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new CircleAlgorithmFactory();
+        }
+
+        private CircleAlgorithmFactory _factory;
+
+        [Test]
+        [TestCase(ShapeType.Rectangle)]
+        [TestCase(ShapeType.Line)]
+        [TestCase(ShapeType.Triangle)]
+        [TestCase(ShapeType.Polygon)]
+        public void WrongShapeTypeTest(ShapeType type)
+        {
+            Assert.That(_factory.TryGetStrategy(type, type, out var res), Is.False);
+            Assert.That(res, Is.Null);
+        }
+
+        [Test]
+        public void CorrectShapeWithAlgorithmTest()
+        {
+            var strategy = new IntersectStrategy(new CircleIntersectsCircle(), new HashSet<ShapeType>
+            {
+                ShapeType.Circle
+            });
+            _factory.AddStrategy(strategy);
+            Assert.That(_factory.TryGetStrategy(ShapeType.Circle, ShapeType.Circle, out var res), Is.True);
+            Assert.That(res, Is.EqualTo(strategy.Algorithm));
+        }
+
+        [Test]
+        public void CorrectShapeNoAlgorithmsTest()
+        {
+            Assert.That(_factory.TryGetStrategy(ShapeType.Circle, ShapeType.Circle, out var res), Is.False);
+            Assert.That(res, Is.Null);
+        }
+    }
+}
---
+++ ShapesFilterTests/AlgorithmSelection/Factory/RectangleAlgorithmFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ShapesFilter.Algorithms;
+using ShapesFilter.AlgorithmSelection.Factory;
+using ShapesFilter.AlgorithmSelection.Strategies;
+using ShapesFilter.Shapes;
+
+namespace ShapesFilterTests.AlgorithmSelection.Factory
+{
+    [TestFixture]
+    public class RectangleAlgorithmFactoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new RectangleAlgorithmFactory();
+        }
+
+        private RectangleAlgorithmFactory _factory;
+
+        [Test]
+        [TestCase(ShapeType.Line)]
+        [TestCase(ShapeType.Circle)]
+        [TestCase(ShapeType.Triangle)]
+        [TestCase(ShapeType.Polygon)]
+        public void WrongShapeTypeTest(ShapeType type)
+        {
+            Assert.That(_factory.TryGetStrategy(type, type, out var res), Is.False);
+            Assert.That(res, Is.Null);
+        }
+
+        [Test]
+        public void CorrectShapeWithAlgorithmTest()
+        {
+            var strategy = new IntersectStrategy(new RectangleIntersectsCircle(), new HashSet<ShapeType>
+            {
+                ShapeType.Circle
+            });
+            _factory.AddStrategy(strategy);
+            Assert.That(_factory.TryGetStrategy(ShapeType.Rectangle, ShapeType.Circle, out var res), Is.True);
+            Assert.That(res, Is.EqualTo(strategy.Algorithm));
+        }
+
+        [Test]
+        public void CorrectShapeNoAlgorithmsTest()
+        {
+            Assert.That(_factory.TryGetStrategy(ShapeType.Rectangle, ShapeType.Circle, out var res), Is.False);
+            Assert.That(res, Is.Null);
+        }
+    }
+}
